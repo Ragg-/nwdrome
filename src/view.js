1ecd7db1ac34432bd92427d09e9d5c700b9360ca
Ragg <Ragg-@users.noreply.github.com>
2014-10-13 02:03:05 +0900

    Add window maximize shortcut key (Return key) in External window.

--- src/view.js
+++ src/view.js
@@ -2,11 +2,19 @@
     var nw      = require("nw.gui"),
         guiWin  = nw.Window.get();
 
+    var ma
+
     window.onkeydown = function (e) {
-        if ((e.ctrlKey || e.metaKey) && e.keyCode === 87) {
+        if (e.keyCode === 27 || ((e.ctrlKey || e.metaKey) && e.keyCode === 87)) {
+            // (Control | Command) + W
             guiWin.hide();
             guiWin.emit("hide");
             e.preventDefault();
+        }
+
+        if (e.keyCode === 13) {
+            // Enter
+            guiWin.maximize();
         }
     };
 }());
